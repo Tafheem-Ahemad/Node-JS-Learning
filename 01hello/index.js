163c8761962cc5970651b5bfc297c5a244c111bb
Tafheem <ahemadtafheem@gmail.com>
2024-09-05 21:04:08 +0530

    URL in http

--- 01hello/index.js
+++ 01hello/index.js
@@ -1,13 +1,17 @@
 const http = require('http');
 const fs = require('fs');
-
+const url = require('url');
 
 const newServer = http.createServer((req,res)=>{
+	if(req.url == "/favicon.ico") return res.end();
 	const log= `${Date.now()} |  ${req.url} new data is entry \n`;
 	fs.appendFile("text.txt",log,(error,data)=>{
 
-		if(req.url == "/"){
-			res.end("Hii I use Node JS , Now In Home Page");
+		const myurl= url.parse(req.url,true);
+		console.log(myurl);
+		
+		if(myurl.query.name){
+			res.end(`Hii I use Node JS , Wecome ${myurl.query.name}`);
 		}else if(req.url == "/about"){
 			res.end("Hii I use Node JS , I am Ahemad");
 		}else{
